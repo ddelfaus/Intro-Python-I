a04e09b66c64cf2ef5dfabc81eef4ad754a02d13
Woober <ddelfaus@gmail.com>
2020-03-09 16:09:50 -0400

    03

--- src/03_modules.py
+++ src/03_modules.py
@@ -13,20 +13,21 @@ import sys
 print(sys.argv[0], 'hello')
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-print(sys.getwindowsversion())
+print('OS platform:', sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print('Python Version:', sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print('Process Id:', os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print('cwd:', os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print('login:', os.getlogin())
