766dff57c658efaf18cee33f5ed3e080a58503ef
Woober <ddelfaus@gmail.com>
2020-03-10 16:07:43 -0400

    fix and cleanup code

--- src/11_args.py
+++ src/11_args.py
@@ -6,6 +6,9 @@
 
 # YOUR CODE HERE
 
+def f1(num1,num2):
+    return num1 + num2
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
@@ -14,15 +17,22 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
-print(f2(1))                    # Should print 1
+def f2(*num):
+    return sum(num)
+
+print(f2(1), "*args")                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
+
+
+
+
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a), "*****")    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -31,7 +41,10 @@ print(f2(a))    # Should print 22
 
 # YOUR CODE HERE
 
-print(f3(1, 2))  # Should print 3
+def f3(num1, num2=1):
+    return num1 +num2
+
+print(f3(1, 2), "f3***")  # Should print 3
 print(f3(8))     # Should print 9
 
 
@@ -45,6 +58,9 @@ print(f3(8))     # Should print 9
 
 # YOUR CODE HERE
 
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print(f"key: {key}, value: {value}")
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -62,4 +78,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,23 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open('./foo.txt') as f:
+    data = f.read()
+    print(data)
+    f.closed
+ 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open('./bar.txt', 'w') as bar:
+    bar.write("random letters fadafrgeaewfawefafwefhtszxcvthjkmoup\n"
+    "random numbers 1093542934192054790234805748329057\n"
+    "random symbols (**#$*$(*@($*#&$&@&#($&&(@*#&$$&*@))!)\n")
+
+
+with open('./bar.txt', 'r') as bar:
+    print(bar.read())
